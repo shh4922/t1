e00c9521cb29dbab4ab8bea0ae2cab2eb34e5d49
hyeonho <gusgh4922@gmail.com>
2023-05-14 18:28:29 +0900

    Initial Commit

---
+++ t1/t1App.swift
@@ -0,0 +1,17 @@
+//
+//  t1App.swift
+//  t1
+//
+//  Created by Hyeonho on 2023/05/14.
+//
+
+import SwiftUI
+
+@main
+struct t1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
